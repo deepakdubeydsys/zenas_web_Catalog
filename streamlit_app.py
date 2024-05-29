d3a773dee55db56d4b76dc19510e5a34c69f12ba
deepakdubeydsys <deepak.dubey@gmail.com>
2024-05-29 14:32:42 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,6 @@
-
+# Import python packages
+import streamlit as st
+#from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+import requests
+st.title("My Parents new healthy Diner:"
