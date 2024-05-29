e5d2378e61e921c213a55f55282fcbbd1e71e160
deepakdubeydsys <deepak.dubey@gmail.com>
2024-05-29 14:43:18 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,9 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests
-st.title("My Parents new healthy Diner:")
+#st.title("My Parents new healthy Diner:")
 
-my_cnx = st.connector.connect(**streamlit.secrets["snowflake"])
+my_cnx = st.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
